706461bb3bfc5abecb1fe8036e32ec4c77bc9c5d
Benjamin Corben <corbenbenjamin@gmail.com>
2018-09-05 13:11:20 +0200

    Mood treated in History activity, enhancements main page

--- app/src/main/java/com/example/benja/moodtracker/History.java
+++ app/src/main/java/com/example/benja/moodtracker/History.java
@@ -1,7 +1,10 @@
 package com.example.benja.moodtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class History extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        //Retrieving the different TextViews that will display depending on the != moods
+        TextView moodView4 = findViewById(R.id.moodView4);
+        TextView moodView3 = findViewById(R.id.moodView3);
+        TextView moodView2 = findViewById(R.id.moodView2);
+        TextView moodView1 = findViewById(R.id.moodView1);
+        TextView moodView0 = findViewById(R.id.moodView0);
+
+        //Making the TextViews disappear
+        moodView4.setVisibility(View.INVISIBLE);
+        moodView3.setVisibility(View.INVISIBLE);
+        moodView2.setVisibility(View.INVISIBLE);
+        moodView1.setVisibility(View.INVISIBLE);
+        moodView0.setVisibility(View.INVISIBLE);
+
+        //Receiving the mood a.k.a the emoji chosen
+        Intent intent = getIntent();
+        int mood = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+
+        //Distinguishing which mood has been selected
+
+        if (mood == 4) {
+            moodView4.setVisibility(View.VISIBLE);
+        }
+        else if (mood == 3) {
+            moodView3.setVisibility(View.VISIBLE);
+        }
+        else if (mood == 2) {
+            moodView2.setVisibility(View.VISIBLE);
+        }
+        else if (mood == 1) {
+            moodView1.setVisibility(View.VISIBLE);
+        }
+        else if (mood == 0) {
+            moodView0.setVisibility(View.VISIBLE);
+        }
+
     }
 }
--- app/src/main/java/com/example/benja/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/benja/moodtracker/MainActivity.java
@@ -1,13 +1,127 @@
 package com.example.benja.moodtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_NUMBER = "com.example.application.EXTRA_NUMBER";
+    int mood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Retrieving all the different moods or buttons that could be selected
+        ImageButton textButton = findViewById(R.id.superHappyTextButton);
+        ImageButton superHappyButton = findViewById(R.id.superHappyButton);
+        ImageButton happyButton = findViewById(R.id.happyButton);
+        ImageButton normalButton = findViewById(R.id.normalButton);
+        ImageButton disappointedButton = findViewById(R.id.disappointedButton);
+        ImageButton sadButton = findViewById(R.id.sadButton);
+        ImageButton historyButton = findViewById(R.id.historyButton);
+        final TextView superHappyBorder = findViewById(R.id.superHappyBorder);
+        final TextView happyBorder = findViewById(R.id.happyBorder);
+        final TextView normalBorder = findViewById(R.id.normalBorder);
+        final TextView disappointedBorder = findViewById(R.id.disappointedBorder);
+        final TextView sadBorder = findViewById(R.id.sadBorder);
+
+        //Setting the borders invisible (as long as no mood has been selected)
+        superHappyBorder.setVisibility(View. INVISIBLE);
+        happyBorder.setVisibility(View. INVISIBLE);
+        normalBorder.setVisibility(View. INVISIBLE);
+        disappointedBorder.setVisibility(View. INVISIBLE);
+        sadBorder.setVisibility(View. INVISIBLE);
+
+
+        //When one clicks on the Add a note button
+        textButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You have clicked on the image", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //When one clicks on the history button
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent historyActivityIntent = new Intent(MainActivity.this, History.class);
+                //Sending the mood a.k.a the emoji slected
+                historyActivityIntent.putExtra(EXTRA_NUMBER, mood);
+                MainActivity.this.startActivity(historyActivityIntent);
+            }
+        });
+
+        //When one clicks on the superHappy emoji
+        superHappyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You have clicked on the smiley", Toast.LENGTH_LONG).show();
+                mood = 4;
+                superHappyBorder.setVisibility(View.VISIBLE);
+                happyBorder.setVisibility(View. INVISIBLE);
+                normalBorder.setVisibility(View. INVISIBLE);
+                disappointedBorder.setVisibility(View. INVISIBLE);
+                sadBorder.setVisibility(View. INVISIBLE);
+
+            }
+        });
+
+        //When one clicks on the happy emoji
+        happyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mood = 3;
+                happyBorder.setVisibility(View.VISIBLE);
+                superHappyBorder.setVisibility(View. INVISIBLE);
+                normalBorder.setVisibility(View. INVISIBLE);
+                disappointedBorder.setVisibility(View. INVISIBLE);
+                sadBorder.setVisibility(View. INVISIBLE);
+
+            }
+        });
+        //When one clicks on the normal emoji
+        normalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mood = 2;
+                normalBorder.setVisibility(View.VISIBLE);
+                superHappyBorder.setVisibility(View. INVISIBLE);
+                happyBorder.setVisibility(View. INVISIBLE);
+                disappointedBorder.setVisibility(View. INVISIBLE);
+                sadBorder.setVisibility(View. INVISIBLE);
+
+            }
+        });
+        //When one clicks on the disappointed emoji
+        disappointedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mood = 1;
+                disappointedBorder.setVisibility(View.VISIBLE);
+                superHappyBorder.setVisibility(View. INVISIBLE);
+                happyBorder.setVisibility(View. INVISIBLE);
+                normalBorder.setVisibility(View. INVISIBLE);
+                sadBorder.setVisibility(View. INVISIBLE);
+            }
+        });
+        //When one clicks on the sad emoji
+        sadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mood = 0;
+                sadBorder.setVisibility(View.VISIBLE);
+                superHappyBorder.setVisibility(View. INVISIBLE);
+                happyBorder.setVisibility(View. INVISIBLE);
+                normalBorder.setVisibility(View. INVISIBLE);
+                disappointedBorder.setVisibility(View. INVISIBLE);
+            }
+        });
     }
 }
