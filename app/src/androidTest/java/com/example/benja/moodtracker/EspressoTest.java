4b90274b92ae5d97f8a6934ca2c4b1dc55008dcb
Benjamin Corben <corbenbenjamin@gmail.com>
2018-10-10 17:10:37 +0200

    Failed Espresso Tests fixed

--- app/src/androidTest/java/com/example/benja/moodtracker/EspressoTest.java
+++ app/src/androidTest/java/com/example/benja/moodtracker/EspressoTest.java
@@ -1,12 +1,18 @@
 package com.example.benja.moodtracker;
 
 
+import android.app.AlertDialog;
+import android.app.Instrumentation;
+import android.content.DialogInterface;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.MonitoringInstrumentation;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.Button;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -15,6 +21,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -88,75 +95,6 @@ public class EspressoTest {
         appCompatImageButton5.perform(click());
 
 
-        ViewInteraction appCompatImageButton6 = onView(
-                allOf(withId(R.id.moodImageButton),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.constraint.ConstraintLayout")),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatImageButton6.perform(click());
-
-        pressBack();
-
-        ViewInteraction appCompatImageButton7 = onView(
-                allOf(withId(R.id.superHappyComment), withContentDescription("MoodTracker"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.constraint.ConstraintLayout")),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatImageButton7.perform(click());
-
-        ViewInteraction editText = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.custom),
-                                childAtPosition(
-                                        withId(R.id.customPanel),
-                                        0)),
-                        0),
-                        isDisplayed()));
-        editText.perform(click());
-
-        ViewInteraction editText2 = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.custom),
-                                childAtPosition(
-                                        withId(R.id.customPanel),
-                                        0)),
-                        0),
-                        isDisplayed()));
-        editText2.perform(replaceText("upset"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(android.R.id.button1), withText("Add"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.buttonPanel),
-                                        0),
-                                3)));
-        appCompatButton.perform(scrollTo(), click());
-
-        ViewInteraction appCompatImageButton9 = onView(
-                allOf(withId(R.id.superHappyComment), withContentDescription("MoodTracker"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.support.constraint.ConstraintLayout")),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatImageButton9.perform(click());
-
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(android.R.id.button2), withText("Cancel"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.buttonPanel),
-                                        0),
-                                2)));
-        appCompatButton2.perform(scrollTo(), click());
 
     }
 
